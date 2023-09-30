dcacfa391e6cb487111198b3e549a5e61664d1de
Matheus Gabriel <math.gsilva07@gmail.com>
2023-09-30 20:03:46 -0300

    docs - documentação do objeto

--- airplaneReserve.java
+++ airplaneReserve.java
@@ -1,3 +1,4 @@
+/** Programa prinicpal para inicar o sistema de reservas */
 public class airplaneReserve {
     public static void main(String[] args) {
         Reserva newReserva = new Reserva(null, null, null, null);
--- reserva.java
+++ reserva.java
@@ -1,10 +1,9 @@
-
-/**
- * @Authors Matheus Gabriel
- *          Emilly Dantas
- *          Gabrielly de Moraes
- * @since 1.0
- */
+/**Classe para reserva de passagens de voos
+ * @author Matheus Gabriel
+ *         Emilly Dantas
+ *         Gabrielly de Moraes
+ * @version 1.3
+*/
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,18 +16,9 @@ public class Reserva {
     private String hora;
     private String data;
 
-    Random idReserva = new Random();
-    int reserva = idReserva.nextInt(9000) + 1000;
     Scanner sc = new Scanner(System.in);
 
-    Random aeroportoRandom = new Random();
-    int aeroportoSorteado = aeroportoRandom.nextInt(3);
-
-    Random idData = new Random();
-    int dataRandom = idData.nextInt(12);
-    Random idHora = new Random();
-    int horaRandom = idHora.nextInt(24);
-
+    /* Método construtor da classe */
     public Reserva(String nomePassageiro, String destino, String hora, String data) {
         setNomePassageiro(nomePassageiro);
         setDestino(destino);
@@ -36,6 +26,7 @@ public class Reserva {
         setData(data);
     }
 
+    /* Método de invocação geral da classe */
     public void iniciar() {
         if (loginUsuario() == true) {
             menu();
@@ -44,6 +35,7 @@ public class Reserva {
         }
     }
 
+    /* Método de espera de processos, ou pausas rápidas entre ações */
     public void carregando(int time) {
         System.out.println("Carregando... \n");
         try {
@@ -53,7 +45,8 @@ public class Reserva {
         }
     }
 
-    // Login
+    /** Método de acesso inicial ao sistema da classe
+     * @return boolean - loginUsuario*/
     public boolean loginUsuario() {
         System.out.print("=== Seja bem-vindo ao Reserva de Voos ===\n" +
                 "- Digite o seu nome\n");
@@ -67,7 +60,7 @@ public class Reserva {
         return true;
     }
 
-    // Menu Principal
+    /* Método de exibição do menu principal */
     public void menuExibir() {
         System.out.println("\t === RESERVA  DE VOOS ===\n" +
                 "\r 1) Comprar Passagem \n" +
@@ -75,7 +68,7 @@ public class Reserva {
                 "\r 3) Sair \n");
     }
 
-    // Menu Aeroportos
+    /* Método de exibição do menu de aeroportos para escolha no método aeroporto() */
     public void exbirMenuAeroportos() {
         System.out.println("\t === AEROPORTOS ===\n" +
                 "\r 1) Guarulhos \n" +
@@ -84,14 +77,16 @@ public class Reserva {
 
     }
 
-    // Menu principal
+    /** Método de invocação do menu principal */
     public void menu() {
         boolean continuar = true;
+        /* Valor usado no laço de repetição */
         do {
             menuExibir();
             int opcMenu = sc.nextInt();
-            carregando(1000);
+            /* Escolha de opção do menu que redireciona para uma outra ação*/
 
+            carregando(1000);
             switch (opcMenu) {
                 case 1:
                     comprarPassagem();
@@ -115,14 +110,18 @@ public class Reserva {
                     }
                     break;
             }
-
         } while (continuar);
     }
 
-    // Exibir Informações
+    /** Método de exibição das informações da reserva feita*/
     public void exibirInformacoes() {
-        if (getNomePassageiro() == null) {
-            System.out.println("Não existe nenhum voo agendado");
+        Random idReserva = new Random();
+        int reserva = idReserva.nextInt(9000) + 1000;
+        /* Número gerado aleatórimente de 4 digítos utilizado para mostrar o id da reserva que foi gerado */
+
+        /* Enquanto não houver um passageiro para um voo, ou um destino, não é exibido a reserva feito. */
+        if (getNomePassageiro() == null || getDestino() == null) {
+            System.out.println("Não existe nenhum voo reservado. tente reservar algum voo.");
         } else {
             System.out.println("\t === RESERVA ===\n" +
                     "\r ID de reserva: " + reserva + "\n" +
@@ -133,7 +132,8 @@ public class Reserva {
         }
     }
 
-    // Escolha de aeroporto
+    /** Método de escolha de aeroporto
+     * @param opcAeroporto int - aeroporto escolhido para fazer o embarque*/
     public void exibirAeroportos(int opcAeroporto) {
         String escolhaAeroporto = " ";
         if (opcAeroporto == 1) {
@@ -151,11 +151,16 @@ public class Reserva {
         }
     }
 
+    /** Método de criação de escolha de um destino que não tem em mente, 
+     * juntamente com um aeroporto que deseja embarcar
+     * @param escolhido String - Escolha do aeroporto selecionado
+     * @param sigla String - Sigla do aeroporto usada no Arraylist e na exibição do destino*/
     public void aeroporto(String escolhido, String sigla) {
         String[] voos = { sigla + "-> Penha/SC", sigla + "-> Fortaleza", sigla + "-> RJ", sigla + "-> Gramado" };
         ArrayList<String> vooReserva = new ArrayList<String>(Arrays.asList(voos));
 
         System.out.println("Temos a seguinte opções para o aeroporto de " + escolhido);
+        /* Geração da lista de voos disponiveis no aeroporto escolhido pelo terminal */
         for (int i = 0; i < vooReserva.size(); i++) {
             System.out.println((i + 1) + ") " + vooReserva.get(i));
         }
@@ -184,7 +189,7 @@ public class Reserva {
         }
     }
 
-    // Comprar Passagem
+    /** Método de compra de passagens*/
     public void comprarPassagem() {
         System.out.println("Comprando passagem... \n"
                 + "Tem algum destino em mente? \n"
@@ -196,6 +201,9 @@ public class Reserva {
             System.out.println("Qual o destino em mente?");
             String destino = sc.next();
 
+            Random aeroportoRandom = new Random();
+            int aeroportoSorteado = aeroportoRandom.nextInt(3);
+            /* Aeroporto sorteado para o embarque do voo que está fazendo a reserva */
             if (aeroportoSorteado == 1 || aeroportoSorteado == 0) {
                 String destinoSintaxe = ("GRU -> " + destino);
                 setDestino(destinoSintaxe);
@@ -222,13 +230,17 @@ public class Reserva {
         }
     }
 
-    // Informações de compra de passagem
+    /** Método de informações da compra de passagem */
     public void infoCompraPassagem() {
         System.out.println("Digite o nome do passageiro: ");
         String nome = sc.next();
         setNomePassageiro(nome);
         carregando(1500);
 
+        Random idData = new Random();
+        int dataRandom = idData.nextInt(12);
+        Random idHora = new Random();
+        int horaRandom = idHora.nextInt(24);
         String newData = (((dataRandom + 1 < 10) ? "0" + (dataRandom + 1) : (dataRandom + 1)) + "/"
                 + ((dataRandom < 10) ? "0" + (dataRandom) : (dataRandom)) + "/2024");
 
@@ -237,10 +249,10 @@ public class Reserva {
         System.out.println("A data de sua viagem é " + newData + " às " + newHorario);
         setData(newData);
         setHora(newHorario);
+
         carregando(1500);
         exibirInformacoes();
         carregando(1500);
-
     }
 
     // Get & Sets
